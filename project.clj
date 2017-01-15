d147b05017e101df7f08bf7bb1080d1634221aa6
Henk Reder <henk@staq.com>
2017-01-15 18:34:56 -0500

    wrote a macro for infix notation and displayed it on my webpage

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]])
+   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-json "0.4.0"]
+                 [http-kit "2.2.0"]
+                 [ring/ring-devel "1.5.1"]
+                 [compojure "1.5.2"]]
+  :main proto.core
+  )
--- src/proto/core.clj
+++ src/proto/core.clj
@@ -1,6 +1,24 @@
 (ns proto.core)
+(use 'org.httpkit.server
+     '[clojure.string :only (split)]
+     )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; before you use this, write some simple infix notation arithmetic
+; in your environment as SOME_MATH. e.g export SOME_MATH='2 * 10'
+
+(defmacro naive-infix []
+  (let [[operand1 op operand2] (split (System/getenv "SOME_MATH") #"\s+")]
+    `(~(symbol op) ~(read-string operand1)  ~(read-string operand2))))
+
+
+(def body (str "look mom, it's infix notation from the environment! " (System/getenv "SOME_MATH") " is " (naive-infix)))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body body
+   })
+
+(run-server handler{:port 8080})
+
+
